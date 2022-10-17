d33a84657bf9354cd89b7f0b7d4582d41e25f867
jtzuya <jtzuya@gmail.com>
2022-10-18 02:59:30 +0800

    Solo Task No.1
    Added Footer & Figma Resource

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Card } from './components/index'
+import { Card, Footer } from './components/index'
 
 const App = () => {
     return (
         <div className="max-w-screen-2xl mr-auto ml-auto">
             <Card />
+            <Footer />
         </div>
     )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <div className="px-6 py-5 bg-react-blue-dark text-xs font-body text-react-blue-light-2">
         &copy; 2022 Jtzuya Development. All rights reserved
-        <p className="pt-2 font-body font-medium tracking-wide uppercase text-[11.5px]">Check Figma design ~ <a className="decoration-none text-blue" target="blank" href="https://www.figma.com/proto/L76SgGAglpcOmwCane0iQe/ReactFacts-(Copy)?page-id=0%3A1&node-id=0%3A4&viewport=309%2C315%2C0.75&scaling=min-zoom">here</a></p>
+        <p className="pt-2 font-body font-medium tracking-wide uppercase text-[11.5px]">Check Figma design ~ <a className="decoration-none text-blue" target="blank" href="https://www.figma.com/proto/HPl4SO0Al5ND0h6M6pFVXj/Digital-Business-Card-(Copy)?page-id=0%3A1&node-id=0%3A129&viewport=773%2C879%2C0.86&scaling=min-zoom">here</a></p>
       </div>
     </footer>
   )
