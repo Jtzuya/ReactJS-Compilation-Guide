7028e7e44a28acd3957f4d7ab5a0327744244f80
jtzuya <jtzuya@gmail.com>
2022-10-17 21:36:07 +0800

    first challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="max-w-screen-2xl mr-auto ml-auto px-6 pt-10">
+            <img src={require('./assets/images/logo192.png')} width="100" height="100" alt="" className="logo" />
+            <h1 className="text-4xl font-bold mt-20">Fun facts about React</h1>
+            <ul className="list-disc text-2xl font-medium mt-5 pl-10">
+                <li>Trash (said by Anonymous Pro Dev)</li>
+                <li>First released in 2013</li>
+                <li>Originally created by Jordan Walke</li>
+                <li>Has over 196+K Stars on <a href="https://github.com/facebook/react">Github</a></li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
 }
 
-export default App;
+export { App }
