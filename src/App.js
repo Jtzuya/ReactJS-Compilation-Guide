862c9840676e5147c6f783153e49b250b175d308
jtzuya <jtzuya@gmail.com>
2022-10-18 02:56:25 +0800

    Solo Task
    No.1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Navbar, MainContent, Footer } from './components/index'
+import { Card } from './components/index'
 
 const App = () => {
     return (
         <div className="max-w-screen-2xl mr-auto ml-auto">
-            <Navbar />
-            <MainContent />
-            <Footer />
+            <Card />
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,11 @@ module.exports = {
     colors: {
       'react-blue-light': '#61DAFB',
       'react-blue-light-2': '#DEEBF8',
-      'react-blue-dark': '#21222A',
-      'react-body': '#33373E',
+      'react-blue-dark': '#1C1C1C',
+      'react-body': '#23252C',
+      'card-light': '#1A1B21',
+      'card-dark': '#161619',
+      'highlight': '#F3BF99',
       'white': '#ffffff',
       'blue': 'blue'
     },
